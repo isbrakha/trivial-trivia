1974757124f92d5b95e3e3eab1134252d470afcd
isbrakha <isaacbrakha14@gmail.com>
2023-12-20 16:32:30 -0500

    loading animation implemented

--- src/pages/Main/Gameplay.jsx
+++ src/pages/Main/Gameplay.jsx
@@ -80,7 +80,7 @@ const Gameplay = () => {
     }
 
     const loading = () => {
-        return isLoading ? <h1>Loading.........</h1> : <h1>Error</h1>;
+        return isLoading ? <h1 className="loading">Loading</h1> : <h1 className="loading">Error</h1>;
       };
 
     return !isLoading ? loaded() : loading()
