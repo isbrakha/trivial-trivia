049ca298b7d3de2a6e7000cd753141a8675fece0
isbrakha <isaacbrakha14@gmail.com>
2023-12-21 13:49:21 -0500

    responsive display changes

--- src/pages/Main/ChooseCategory.jsx
+++ src/pages/Main/ChooseCategory.jsx
@@ -11,31 +11,31 @@ const Category = () => {
 
     return (
         <section className="button-grandpa">
-            <div className="buttons-wrapper" onClick={() => handleClick("history")}>
+            <div className="buttons-wrapper-left" onClick={() => handleClick("history")}>
                 <h2 className= "buttons">History</h2>
             </div>
             <div className="buttons-wrapper" onClick={() => handleClick("sports_and_leisure")}>
                 <h2 className= "buttons">Sports and Leisure</h2>
             </div>
-            <div className="buttons-wrapper" onClick={() => handleClick("music")}>
+            <div className="buttons-wrapper-left" onClick={() => handleClick("music")}>
                 <h2 className= "buttons">Music</h2>
             </div>
             <div className="buttons-wrapper" onClick={() => handleClick("film_and_tv")}>
                 <h2 className= "buttons">Film and TV</h2>
             </div>
-            <div className="buttons-wrapper" onClick={() => handleClick("arts_and_literature")}>
+            <div className="buttons-wrapper-left" onClick={() => handleClick("arts_and_literature")}>
                 <h2 className= "buttons">Arts and Literature</h2>
             </div>
             <div className="buttons-wrapper" onClick={() => handleClick("society_and_culture")}>
                 <h2 className= "buttons">Society and Culture</h2>
             </div>
-            <div className="buttons-wrapper" onClick={() => handleClick("science")}>
+            <div className="buttons-wrapper-left" onClick={() => handleClick("science")}>
                 <h2 className= "buttons">Science</h2>
             </div>
             <div className="buttons-wrapper" onClick={() => handleClick("geography")}>
                 <h2 className= "buttons">Geography</h2>
             </div>
-            <div className="buttons-wrapper" onClick={() => handleClick("food_and_drink")}>
+            <div className="buttons-wrapper-left" onClick={() => handleClick("food_and_drink")}>
                 <h2 className= "buttons">Food and Drink</h2>
             </div>
             <div className="buttons-wrapper" onClick={() => handleClick("general_knowledge")}>
